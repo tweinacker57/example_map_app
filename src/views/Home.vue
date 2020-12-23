f2a13730d1caa42d7eaf8b158cc421f07cf900f3
Tyler Weinacker <tweinacker57@gmail.com>
2020-12-22 21:51:17 -0500

    Add popup to a marker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div id="map"></div>
-    <h1>{{ message }}</h1>
+    <h1>{{ places }}</h1>
   </div>
 </template>
 
@@ -16,23 +16,42 @@ body {
   bottom: 0;
   width: 100%;
 }
+#marker {
+  background-image: url("https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg");
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.mapboxgl-popup {
+  max-width: 200px;
+}
 </style>
 
 <script>
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      places: [{ lng: -71.25336029235405, lat: 42.07020495062423 }],
     };
   },
   mounted: function () {
     mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    var house = [-71.25336029235405, 42.07020495062423];
     var map = new mapboxgl.Map({
       container: "map", // container id
-      style: "mapbox://styles/mapbox/streets-v11", // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      style: "mapbox://styles/mapbox/light-v10", // style URL
+      center: house, // starting position [lng, lat]
+      zoom: 18, // starting zoom
     });
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "This is the house I grew up in"
+    );
+    var el = document.createElement("div");
+    el.id = "marker";
+    new mapboxgl.Marker(el).setLngLat(house).setPopup(popup).addTo(map);
   },
   methods: {},
 };
